b074257fc8688c438518cfbf807deb88047ee18f
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-02 13:45:56 +0600

    Login and Register Page Created

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import LoginImage from '../../assets/images/login.jpg';
+import './Login.css';
+import Google from '../../assets/icons/google.png';
+import Github from '../../assets/icons/github.png';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h3>Please, Login</h3>
+        <div className='login container'>
+            <div>
+                <form>
+                    <h3>Please Login</h3>
+                    <hr />
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" name='email' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" name='password' class="form-control" id="exampleInputPassword1" required />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+
+                <hr />
+                <div>
+                    <p>If you don't have account <Link to="/register" className='sublink'>Register Now</Link> or 👇 </p>
+                </div>
+                <div className='text-center'>
+                    <div className='google'><img src={Google} height={20} className='me-2' alt="" /><span>Sign in with Google</span></div>
+                    <div className='github'><img src={Github} height={20} className='me-2' alt="" /><span>Sign in with Github</span></div>
+                </div>
+            </div>
+            <div>
+                <div className='login-image'>
+                    <img src={LoginImage} className='img-fluid' alt="" />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import './Register.css';
+import RegisterImage from '../../assets/images/register.jpg';
 
 const Register = () => {
     return (
-        <div>
-            <h3>Register Page</h3>
+        <div className='login container'>
+            <div>
+                <form>
+                    <h3>Please Register as New</h3>
+                    <hr />
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Name</label>
+                        <input type="text" name='name' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" name='email' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" name='password' class="form-control" id="exampleInputPassword1" required />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Photo URL</label>
+                        <input type="text" name='photoURL' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                    </div>
+                    <button type="submit" class="btn btn-success">Register</button>
+                </form>
+            </div>
+            <div>
+                <div className='login-image'>
+                    <img src={RegisterImage} height={100} className='img-fluid' alt="" />
+                </div>
+            </div>
         </div>
     );
 }
