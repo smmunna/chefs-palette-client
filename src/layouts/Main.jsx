840be8e0567056a7fd231acf5617c2eafc6e840c
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-02 22:21:46 +0600

    Update Main.jsx

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet,ScrollRestoration  } from 'react-router-dom';
 
 const Main = () => {
     return (
@@ -9,6 +9,7 @@ const Main = () => {
             <Header/>
             <Outlet/>
             <Footer/>
+            <ScrollRestoration />
         </div>
     );
 }
