81d7bc096d02fe135330f9aa5bcab52be559f305
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-02 20:19:07 +0600

    Footer Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+    const d = new Date();
+    let year = d.getFullYear();
     return (
-        <div>
-            <p>Coyright 2023&copy;</p>
+        <div className='d-flex justify-content-center align-items-center footer'>
+
+            <div>
+                <h3 className="display-6 fw-semibold text-white">Chef's Palette</h3>
+                <p>Designed & Developed by Minhazul Abedin Munna</p>
+                <p>Copyright PHero {year}&copy;</p>
+            </div>
         </div>
     );
 }
