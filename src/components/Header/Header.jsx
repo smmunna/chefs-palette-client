31b4d388d246a2af00ea583c650a5e246541bc31
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-02 08:25:36 +0600

    Header Completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Header.css';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import BrandIcon from '../../assets/icons/brandicon.png';
 
 const Header = () => {
     return (
-        <div>
-            <h3>Header</h3>
+        <div className='navbar-bg'>
+            <Navbar collapseOnSelect expand="lg">
+                <Container>
+                    <img
+                        src={BrandIcon}
+                        width="40"
+                        height="40"
+                        alt="React Bootstrap logo"
+                    />
+                    <Navbar.Brand href="#home" className='ms-2 brand'>Chef's Palette</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link>
+                                <Link to="/">Home</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to="/blog">Blog</Link>
+                            </Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Button className='btn btn-warning'><Link to="/login">Login</Link></Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/Router.jsx'
 
