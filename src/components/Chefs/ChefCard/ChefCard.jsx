2afee5fc676aae2ba2f28acaeae5f92cdb0a9ce4
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-02 14:29:36 +0600

    Update

--- src/components/Chefs/ChefCard/ChefCard.jsx
+++ src/components/Chefs/ChefCard/ChefCard.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './ChefCard.css';
 
-const ChefCard = () => {
+const ChefCard = ({ chef }) => {
+    const { id, name, picture, likes, recipes, years_of_experience } = chef;
     return (
-        <div>
-            <h3>ChefCard</h3>
+        <div class="col">
+            <div class="card h-100">
+                <img src={picture} class="card-img-top img-fluid" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <p class="card-text">Number of recipes: {recipes.length}</p>
+                    <div className='chef-priority'>
+                        <div>
+                            <p>Expericence: {years_of_experience}</p>
+                        </div>
+                        <div>
+                            Likes {likes}
+                        </div>
+                    </div>
+                    {/* View Chef */}
+                    <div>
+                        <button className='btn btn-warning mt-2'>View Recipes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Chefs/Chefs.jsx
+++ src/components/Chefs/Chefs.jsx
@@ -7,12 +7,14 @@ const Chefs = () => {
     return (
         <div>
             <h3 className='my-4 text-center display-4 fw-semibold'>Our Chef's</h3>
-            {
-                data.map(chef=><ChefCard
-                    key={chef.id}
-                    chef={chef}
-                />)
-            }
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                {
+                    data.map(chef => <ChefCard
+                        key={chef.id}
+                        chef={chef}
+                    />)
+                }
+            </div>
         </div>
     );
 }
