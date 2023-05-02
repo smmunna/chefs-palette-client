a944ca7b671f887326775e15921d54ef924618a6
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-02 09:34:34 +0600

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
+import HomeHeader from './HomeHeader/HomeHeader';
 
 const Home = () => {
     return (
         <div>
             <h3>I am from Home</h3>
+            <HomeHeader/>
         </div>
     );
 }
